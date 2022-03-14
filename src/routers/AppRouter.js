5341788eefe56ba4297cf837355e984bb29735f0
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-14 20:35:22 -0300

    route protection - public and private

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
     Redirect
   } from "react-router-dom";
 
 
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 import { LoginScreen } from '../components/auth/LoginScreen';
+
 import { startChecking } from '../actions/auth';
+
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 
 
@@ -18,19 +21,34 @@ export const AppRouter = () => {
   
   const dispatch = useDispatch();
 
+  const { checking, uid } = useSelector( state => state.auth );
+
   useEffect(() => {
 
     dispatch( startChecking() );
 
   }, [ dispatch ]);
+
+  if ( checking ) {
+    return ( <h5>Espere...</h5> );
+  }
   
   return (
     <Router>
         <div>
             <Switch>
 
-                <Route exact path='/login' component={ LoginScreen }/>
-                <Route exact path='/' component={ CalendarScreen }/>
+                <PublicRoute 
+                  exact path='/login' 
+                  component={ LoginScreen }
+                  isAutenticated={ !!uid }
+                />
+
+                <PrivateRoute 
+                  exact path='/' 
+                  component={ CalendarScreen }
+                  isAutenticated={ !!uid }
+                />
 
                 <Redirect to='/' />
 
