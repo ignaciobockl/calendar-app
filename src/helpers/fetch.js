d5bd31f71612d4a39289e310f917123f1b0c178a
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-14 13:08:12 -0300

    startLogin

---
+++ src/helpers/fetch.js
@@ -0,0 +1,24 @@
+
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+export const fetchWithoutToken = ( endpoint, data, method = 'GET' ) => {
+
+    const url = `${ baseURL }/${ endpoint }`;
+
+    if ( method === 'GET' ) {
+        return fetch( url );
+    } else {
+        return fetch( url, {
+            method,
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify( data )
+        });
+    }
+
+}
+
+
+
--- src/types/types.js
+++ src/types/types.js
@@ -2,13 +2,21 @@
 
 export const types = {
 
-    uiOpenModal: '[ui] Open Modal',
-    uiCloseModal: '[ui] Close Modal',
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authLogin: '[auth] Start login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
 
     eventSetActive: '[event] Set Active',
     eventAddNew: '[event] Add new',
     eventClearActiveEvent: '[event] Clear active event',
     eventUpdate: '[event] Event updated',
     eventDeleted: '[event] Event deleted',
+
+    uiOpenModal: '[ui] Open Modal',
+    uiCloseModal: '[ui] Close Modal',
+
 
 }
