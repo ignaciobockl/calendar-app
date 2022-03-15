58c35f3767e4bbe0b16ab2cbafbcfb6fc06d944a
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-15 16:08:40 -0300

    startLogin successful

---
+++ src/tests/actions/auth.test.js
@@ -0,0 +1,50 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import '@testing-library/jest-dom';
+
+import { startLogin } from '../../actions/auth';
+
+import { types } from '../../types/types';
+
+
+
+const middlewares = [ thunk ];
+const mockStore = configureStore( middlewares );
+
+const initState = {};
+let store = mockStore( initState );
+
+Storage.prototype.setItem = jest.fn();
+
+
+describe('Pruebas en las acciones de auth', () => {
+
+    beforeEach( () => {
+        store = mockStore( initState );
+        jest.clearAllMocks();
+    });
+
+    test('startLogin correcto', async() => {
+
+        await store.dispatch( startLogin('test@gmail.com', '132456') );
+
+        const actions = store.getActions();
+
+        expect( actions[0] ).toEqual({
+            type: types.authLogin,
+            payload: {
+                uid: expect.any(String),
+                name: expect.any(String)
+            }
+        });
+
+        expect( localStorage.setItem ).toHaveBeenCalledWith('token', expect.any(String));
+        expect( localStorage.setItem ).toHaveBeenCalledWith('token-init-date', expect.any(Number));
+
+        // token = localStorage.setItem.mock.calls[0][1]
+        // console.log(localStorage.setItem.mock.calls[0][1])
+
+    });
+
+});
