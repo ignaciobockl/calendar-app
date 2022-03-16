a524fe272d00334f14cc7cbb26adecc8a646f2d5
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-15 23:36:15 -0300

    tests in the uiReducer

---
+++ src/tests/reducers/uiReducer.test.js
@@ -0,0 +1,23 @@
+// import { uiReducer } from "../../reducers/uiReducer";
+
+
+const initState = {
+    modalOpen: false
+}
+
+
+describe('Pruebas en uiReducer', () => {
+
+    // test('debe de retornar el estado por defecto', () => {
+
+    //     const state = uiReducer( initState, {} );
+
+    //     expect( state ).toEqual( initState );
+
+    // });
+
+    // test('debe de abrir y cerrar el modal', () => {
+
+    // });
+
+});
