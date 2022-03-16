a234656905f7ba456435b752dd75389b919824df
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-16 11:11:07 -0300

    Tests in the authReducer.js

---
+++ src/tests/reducers/authReducer.test.js
@@ -0,0 +1,43 @@
+import { authReducer } from "../../reducers/authReducer";
+
+// import { types } from "../../types/types";
+
+
+// const initialState = {
+//     checking: true
+// }
+
+// let action = {};
+
+
+describe('Pruebas en el authReducer.js', () => {
+
+    // test('debe de retornar el estado por defecto', () => {
+
+    //     const state = authReducer( initialState, action );
+        
+    //     expect( state ).toEqual( initialState );
+
+    // });
+
+    // test('debe de hacer el login de usuario correctamente', () => {
+
+    //     action = {
+    //         type: types.authLogin,
+    //         payload: {
+    //             uid: '8332',
+    //             name: 'Esmeralda'
+    //         }
+    //     }
+
+    //     const state = authReducer( initialState, action );
+        
+    //     expect( state ).toEqual({
+    //         checking: false,
+    //         uid: '8332',
+    //         name: 'Esmeralda'
+    //     });
+
+    // });
+
+});
