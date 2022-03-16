45f97161d88f0d0d28d0e9faae68048aa0680bcf
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-16 17:16:52 -0300

    must call the dispatch of the Login

--- src/tests/components/auth/LoginScreen.test.js
+++ src/tests/components/auth/LoginScreen.test.js
@@ -8,6 +8,13 @@ import '@testing-library/jest-dom';
 
 import { LoginScreen } from '../../../components/auth/LoginScreen';
 
+import { startLogin } from '../../../actions/auth';
+
+
+jest.mock('../../../actions/auth', () => ({
+    startLogin: jest.fn()
+}));
+
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -29,6 +36,30 @@ describe('Pruebas en <LoginScreen />', () => {
     test('debe de mostrarse correctamente', () => {
 
         expect ( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('debe de llamar el dispatch del Login', () => {
+
+        wrapper.find('input[name="lEmail"]').simulate('change', {
+            target: {
+                name: 'lEmail',
+                value: 'cami@gmail.com'
+            }
+        });
+
+        wrapper.find('input[name="lPassword"]').simulate('change', {
+            target: {
+                name: 'lPassword',
+                value: '123456'
+            }
+        });
+
+        wrapper.find('form').at(0).prop('onSubmit')({
+            preventDefault(){}
+        });
+
+        expect( startLogin ).toHaveBeenCalledWith('cami@gmail.com', '123456');
 
     });
 
