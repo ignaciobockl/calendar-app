7748fb6b00b86beed258cd28856cf6e9f0daf44d
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-16 17:34:56 -0300

    No log if passwords are different

--- src/tests/components/auth/LoginScreen.test.js
+++ src/tests/components/auth/LoginScreen.test.js
@@ -3,16 +3,22 @@ import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import Swal from 'sweetalert2';
 
 import '@testing-library/jest-dom';
 
 import { LoginScreen } from '../../../components/auth/LoginScreen';
 
-import { startLogin } from '../../../actions/auth';
+import { startLogin, startRegister } from '../../../actions/auth';
 
 
 jest.mock('../../../actions/auth', () => ({
-    startLogin: jest.fn()
+    startLogin: jest.fn(),
+    startRegister: jest.fn()
+}));
+
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn()
 }));
 
 
@@ -60,6 +66,48 @@ describe('Pruebas en <LoginScreen />', () => {
         });
 
         expect( startLogin ).toHaveBeenCalledWith('cami@gmail.com', '123456');
+
+    });
+
+    test('No hay registro si las contraseñas son diferentes', () => {
+
+        wrapper.find('input[name="rName"]').simulate('change', {
+            target: {
+                name: 'rName',
+                value: 'Camii'
+            }
+        });
+
+        wrapper.find('input[name="rEmail"]').simulate('change', {
+            target: {
+                name: 'rEmail',
+                value: 'cami@gmail.com'
+            }
+        });
+
+        wrapper.find('input[name="rPassword1"]').simulate('change', {
+            target: {
+                name: 'rPassword1',
+                value: '123456'
+            }
+        });
+
+        wrapper.find('input[name="rPassword2"]').simulate('change', {
+            target: {
+                name: 'rPassword2',
+                value: '132456'
+            }
+        });
+
+        wrapper.find('form').at(1).prop('onSubmit')({
+            preventDefault(){}
+        });
+
+        // startRegister should not be called if the passwords are not the same
+        expect( startRegister ).not.toHaveBeenCalled();
+
+        // sweetalert2 must be called in case the passwords are different
+        expect( Swal.fire ).toHaveBeenCalledWith('Error', 'Passwords do not match.', 'error');
 
     });
 
