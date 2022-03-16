8240a36c6f1f4851381f6019fe871c9ff6104501
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-16 13:50:01 -0300

    Testing on <AppRouter />

---
+++ src/tests/routers/AppRouter.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import '@testing-library/jest-dom';
+
+import { AppRouter } from '../../routers/AppRouter';
+
+
+const middlewares = [ thunk ];
+const mockStore = configureStore( middlewares );
+
+// store.dispatch = jest.fn();
+
+
+
+describe('Pruebas en <AppRouter />', () => {
+
+    test('debe de mostrar el espere...', () => {
+
+        const initState = {
+            auth: {
+                checking: true
+            }
+        };
+        const store = mockStore( initState );
+
+        const wrapper = mount(
+            <Provider store={ store } >
+                <AppRouter />
+            </Provider>
+        );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('h5').exists() ).toBe( true ); // same check without snapshot
+
+    });
+
+});
