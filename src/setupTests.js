eb0eb21d2742c817dad270b2020b2753ffd4e9a9
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-16 18:51:06 -0300

    Tests on <CalendarModal />

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,3 +7,6 @@ import { createSerializer } from 'enzyme-to-json';
 Enzyme.configure({ adapter: new Adapter() });
 
 expect.addSnapshotSerializer( createSerializer({ mode: 'deep' }) );
+
+
+HTMLCanvasElement.prototype.getContext = () => {};
---
+++ src/tests/components/calendar/CalendarModal.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import moment from 'moment';
+
+import '@testing-library/jest-dom';
+
+import { CalendarModal } from '../../../components/calendar/CalendarModal';
+
+
+// mock a la accion eventSetActive
+// jest.mock('../../../actions/events', () => ({
+//     eventSetActive: jest.fn(),
+//     eventStartLoading: jest.fn()
+// }));
+
+// localStorage
+// Storage.prototype.setItem = jest.fn();
+
+
+const middlewares = [ thunk ];
+const mockStore = configureStore( middlewares );
+
+const now = moment().minutes(0).seconds(0).add(1,'hours'); // 3:00:00
+const nowPlus1 = now.clone().add(1, 'hours');
+
+const initState = {
+    calendar: {
+        events: [],
+        activeEvent: {
+            title: 'Cumple Lu',
+            notes: 'Comprar Pizzetas',
+            start: now.toDate(),
+            end: nowPlus1.toDate()
+        }
+    },
+    auth: {
+        checking: false,
+        uid: '5107',
+        name: 'Lu'
+    },
+    ui: {
+        modalOpen: true
+    }
+};
+const store = mockStore( initState );
+store.dispatch = jest.fn();
+
+const wrapper = mount(
+    <Provider store={ store } >
+        <CalendarModal />
+    </Provider>
+);
+
+
+
+describe('Pruebas en <CalendarModal />', () => {
+
+    test('debe de mostrar el modal', () => {
+
+        expect( wrapper.find('.modal').exists() ).toBe( true );
+
+        expect( wrapper.find('Modal').prop('isOpen') ).toBe( true );
+
+    });
+
+});
